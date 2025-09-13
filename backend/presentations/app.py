72d17c799437794f92ec82d7775b3c8cfecf64ba
Mikhail Khapkov <xmeloff12326@gmail.com>
2025-09-13 11:10:32 +0300

    added proper auth with tg

--- backend/presentations/app.py
+++ backend/presentations/app.py
@@ -1,7 +1,7 @@
 
 # fastapi endpoints
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
@@ -23,6 +23,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="todo miniapp", lifespan=lifespan)
 
+# через этот мы не получаем объект, а просто защищаем ручку
+protected_router = APIRouter(
+    dependencies=[Depends(get_current_user)]
+)
+# через этот нужно оставить Depends в параметрах для получения объекта пользователя внутри функции
+router = APIRouter() 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -31,14 +38,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/api/health")
+class Message(BaseModel):
+    text: str
+
+# эта ручка не защищена
+@router.get("/api/health")
 async def health():
     return {"status": "ok"}
 
-@app.get("/api/auth-data")
+@protected_router.post("/api/message")
+async def send_message(message: Message):
+    # Логика отправки сообщения.
+    # Обратите внимание, что здесь нет доступа к объекту user.
+    # Эта функция просто выполнится, если get_current_user не вызовет исключение.
+    return {"status": "ok", "message": f"Protected message '{message.text}' sent."}
+
+@router.get("/api/auth-data")
 async def auth(user: WebAppUser = Depends(get_current_user)):
     logger.info(f"User data: {vars(WebAppUser)}")
     await add_user(user.id)
     return await get_tasks(user.id)
+
+app.include_router(protected_router)
+app.include_router(router)
 
 #app.mount("/", StaticFiles(directory="../frontend", html = True), name="view")
